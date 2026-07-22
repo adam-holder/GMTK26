698fef77ec98818aa31acd3d939e55cfe81316f2
Annikatt <anneke256@gmail.com>
2026-07-22 15:17:20 -0400

    basic tilemap and player added

---
+++ Scripts/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+
+const speed = 300.0
+
+
+func _physics_process(delta):
+	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")*speed
+	move_and_slide()
