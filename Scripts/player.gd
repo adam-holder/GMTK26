66591b60ccea9fd10581947181d13a62e0491001
Annikatt <anneke256@gmail.com>
2026-07-23 15:30:47 -0400

    Player animations and temp colors

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,24 @@
 extends CharacterBody2D
 
+@onready var body = $Body
 
 const speed = 50.0
 
+func _ready():
+	pass
 
 func _physics_process(delta):
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")*speed
 	move_and_slide()
+	if velocity.length() < .01:
+		body.stop()
+	elif abs(velocity.x) > abs(velocity.y):
+		if velocity.x > 0:
+			body.play("walk_right")
+		else:
+			body.play("walk_left")
+	else:
+		if velocity.y > 0:
+			body.play("walk_down")
+		else:
+			body.play("walk_up")
