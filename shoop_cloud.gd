f78625ac062e5b81b6974c7e78d386f73cff54f8
Annikatt <anneke256@gmail.com>
2026-07-26 12:36:54 -0400

    layers

--- shoop_cloud.gd
+++ shoop_cloud.gd
@@ -4,6 +4,14 @@ extends Node2D
 
 func _ready() -> void:
 	position = position + Vector2(randi_range(-200,0),randi_range(-50,200))
+	if randf() < .01:
+		modulate = Color(1.0, 0.766, 0.73, 1.0)
+	if randf() < .005:
+		modulate = Color(1.0, 0.89, 0.61, 1.0)
+	if randf() < .3:
+		$Sprite2D.flip_h = true
+	var scal = randf_range(1.0,2.0)
+	scale = Vector2(scal,scal)
 
 func _process(delta: float) -> void:
 	position += Vector2(shoopspeed,0) * delta
