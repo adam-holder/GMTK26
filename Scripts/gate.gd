9ad9236d62840257c02a7f0a54c3c81c3213a8ca
Annikatt <anneke256@gmail.com>
2026-07-25 20:42:55 -0400

    omg lots of stuff

--- Scripts/gate.gd
+++ Scripts/gate.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
 @export var required_sheep = 0
+@export var vertical = false
 
 func _ready():
 	$Requirement.text = str(required_sheep)
 	Globals.shoop_collected.connect(shoop_check)
+	if vertical: $Sprite.play("vertBefore")
+	else: $Sprite.play("horizBefore")
+		
 
 func shoop_check():
 	required_sheep -= 1
 	#$Requirement.text = str(required_sheep)
 	if required_sheep <= 0:
 		Globals.shoop_collected.disconnect(shoop_check)
-		$Clouds.queue_free()
+		if vertical: $Sprite.play("vertAfter")
+		else: $Sprite.play("horizAfter")
 		$Requirement.queue_free()
 		$StaticBody2D.queue_free()
